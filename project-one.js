a84464e1c27b1bb18e71ac0a6e2d90fc73e3a635
Yancy <yancymueller@Yancys-MacBook-Air.local>
2020-12-21 15:40:36 -0600

    updated js pseudocode

---
+++ project-one.js
@@ -0,0 +1,21 @@
+
+
+
+//          MVP GOALS
+// Make Start Game button clickable to begin game
+// //create click events with individual cards
+// When the Start Button is clicked start the timer
+// Make cards clickable
+//When cards are clicked flip over to reveal pictures
+// Leave one card flipped, and if second flipped card doesnt match turn them both back over
+    // If the two cards match flip the both back over 
+// Make restart game button clickable
+//When restart button is clicked start game over
+// Alert player when they have won the game!
+
+
+//        STRETCH GOALSs
+// Keep track of players score 
+// use input to take players name and return it in the game 
+//Add API of old school classic game show hosts/ game show style
+//send winning cards to a deck
